c9677dd823d9e6155c9f628b7c4f8fc91b70f108
Dennis <dennishou132@gmail.com>
2016-08-05 23:46:02 +1000

    Let's try this again:
    Task 1.3 -- 70% Complete
    	- Home page
    	- FAQ page
    	- Bootstrap + PHP

---
+++ WDA-User/assets/content/faq-student-content.php
@@ -0,0 +1,82 @@
+<!-- FAQ CONTENT -->
+
+<!-- STUDENTS -->
+<div class="faq-header">
+  <h5 class="faq-header-text">
+    Students
+  </h5>
+</div>
+
+<div class="panel-group">
+
+  <div class="panel panel-default">
+
+    <!-- Panel -->
+    <div class="panel-heading" data-toggle="collapse" href="#faq-student-1">
+      <h4 class="panel-title">
+        How do I connect to RMIT's Wi-Fi?
+      </h4>
+    </div>
+    <div id="faq-student-1" class="panel-collapse collapse">
+      <div class="panel-body">
+        1. Choose RMIT-University in your wireless network list.</br>
+        2. Enter your RMIT student ID and password.
+      </div>
+      <div class="panel-footer">
+        For more information on connecting to RMIT's wireless network, go to
+        <a href="https://community.its.rmit.edu.au/t5/IT-Knowledge-Base/Getting-Connected-to-WiFi-at-RMIT/ba-p/1270">
+        Getting Connected at RMIT</a>.
+      </div>
+    </div>
+
+
+    <!-- Panel -->
+    <div class="panel-heading" data-toggle="collapse" href="#faq-student-2">
+      <h4 class="panel-title">
+        How can I use other universities's Wi-Fi as an RMIT student?
+      </h4>
+    </div>
+    <div id="faq-student-2" class="panel-collapse collapse">
+      <div class="panel-body">
+        <bold>eduroam</bold> is an independent wireless network that allows mobility
+        between educational institutions for authorised users.
+        RMIT students and staff will only need to connect to eduroam
+        when off campus at another institution. Instructions on accessing
+        eduroam can be found in <a href="https://community.its.rmit.edu.au/t5/IT-Knowledge-Base/Getting-Connected-to-WiFi-at-RMIT/ba-p/1270">
+        Getting Connected at RMIT</a>.
+      </div>
+    </div>
+
+
+    <!-- Panel -->
+    <div class="panel-heading" data-toggle="collapse" href="#faq-student-3">
+      <h4 class="panel-title">
+        How do I change my password?
+      </h4>
+    </div>
+    <div id="faq-student-3" class="panel-collapse collapse">
+      <div class="panel-body">
+        Simply go to <a href="https://sspr.rmit.edu.au/sspr-chall/private/Login">
+        Self Service Password Reset</a>.
+      </div>
+    </div>
+
+
+    <!-- Panel -->
+    <div class="panel-heading" data-toggle="collapse" href="#faq-student-4">
+      <h4 class="panel-title">
+        I forgot my password, help!
+      </h4>
+    </div>
+    <div id="faq-student-4" class="panel-collapse collapse">
+      <div class="panel-body">
+        Reset your password at <a href="https://sspr.rmit.edu.au/sspr-main">
+        Self Service Password Reset</a>.
+      </div>
+    </div>
+
+
+
+
+  </div>
+</div>
