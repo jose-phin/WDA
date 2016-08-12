abeca4e8a7e97846ad6be0c2b4ab7558bdb4bbee
Chloe Smith <chloesmith747@gmail.com>
2016-08-12 12:48:01 +1000

    updated ticket endpoints

--- API/endpoints/new.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-  try {
-    echo 'Creating Ticket with params\n';
-    echo var_dump($_REQUEST);
-  } catch (Exception $e) {
-    echo json_encode(Array('error' => $e->getMessage()));
-  }
-?>
