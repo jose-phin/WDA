3f863db74853c5a58c1b0f0b8de3313b5401dd41
Chloe Smith <chloesmith747@gmail.com>
2016-08-19 12:24:55 +1000

    Got the new ticket api working with the test database

--- API/endpoints/ticket/new.php
+++ API/endpoints/ticket/new.php
@@ -1,9 +1,21 @@
 <?php
 
-  try {
-    echo 'Creating Ticket with params\n';
-    echo var_dump($_REQUEST);
-  } catch (Exception $e) {
-    echo json_encode(Array('error' => $e->getMessage()));
-  }
+  include($_SERVER['DOCUMENT_ROOT'].'\db\DatabaseHandler.php');
+
+  //initialise the Database and the Database Handler
+    $db = new DatabaseHandler("TestDatabase", TRUE);
+
+    $db->createUser("John", "Doe", "poo@ko.com", FALSE);
+
+    $testUser = $db->getUserByEmail("poo@ko.com");
+    $postData = json_decode(file_get_contents('php://input'), TRUE);
+
+    $db->createTicket($postData['osType'], $postData['primaryIssue'], $postData['additionalNotes'], "pending", $testUser['user_id']);
+    echo var_dump($db->getTicket('1'));
+
+  // create new DatabaseHandler($name, TRUE)
+  // Dbhandler->setUpTables()
+  // db->createUser($fName, $lName, $email, FALSE) <- remove this later
+  // db->createTcket($osType, $primatyIssue, $assitionalNotes, $status, $userID)
+  // send response back to user with success/fail
 ?>
