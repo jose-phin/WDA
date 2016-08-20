04dd6c201e1b3882e5157c41e2b30e3f569f982a
Dennis <dennishou132@gmail.com>
2016-08-20 16:41:23 +1000

    Laying down basic layout on user's ticket page

---
+++ WDA-User/assets/content/individual-ticket.php
@@ -0,0 +1,27 @@
+<!-- <div class="faq-header">
+  <h5 class="faq-header-text">
+    Students
+  </h5>
+</div> -->
+
+<div class="panel-group">
+
+  <div class="panel panel-default">
+
+      <div class="panel-body">
+        <p class="ticket-info-header-text">
+          Full Name
+        </p>
+        <p>
+          John Appleseed
+        </p>
+        1. Choose RMIT-University in your wireless network list.</br>
+        2. Enter your RMIT student ID and password.
+      </div>
+      <div class="panel-footer panel-comment">
+        For more information on connecting to RMIT's wireless network, go to
+      </div>
+
+
+  </div>
+</div>
---
+++ WDA-User/ticket.php
@@ -0,0 +1,79 @@
+<!-- Main landing page -->
+<!DOCTYPE html>
+
+<!-- Identifies user's location in website -->
+<?php
+    $currPage = "ticket";
+?>
+
+<html lang="en">
+
+<head>
+
+    <!-- Site metadata -->
+    <title>Your Ticket - ITS Ticketing System | RMIT University </title>
+
+    <!-- Global head items such as jQuery, Bootstrap, CSS, etc -->
+    <?php include "./assets/head_items.php" ?>
+    <link rel="stylesheet" href="./assets/css/individual-ticket-style.css">
+
+</head>
+
+<body>
+    <div class="home-body content">
+
+        <div class="site-wide-container container">
+
+            <!-- Navigation Bar -->
+            <?php include "navbar.php"; ?>
+
+            <div class="hero main-hero">
+
+                <h2>
+                    {{Ticket Title}}
+                </h2>
+
+                <div class="row">
+                    <div class="col-sm-2">
+                        <h4>
+                            {{Ticket ID: #12345}}
+                        </h4>
+                    </div>
+                    <!-- Ticket status -->
+                    <div class="col-sm-2">
+                        <span class="status status-unresolved">Unresolved</span>
+                    </div>
+                </div>
+
+                <!-- Instantiate Bootstrap's md-10 grid -->
+                <div class="row">
+                    <div class="col-md-10">
+                        <!-- User ticket contents -->
+                        <?php include './assets/content/individual-ticket.php' ?>
+
+                        <!-- Return home button -->
+                        <div class="row btn-margin-fix">
+                            <div class="col-sm-12 col-md-2 btn-col ">
+                                <button class="btn btn-secondary follow-up-home-button">Return Home</button>
+                            </div>
+                        </div>
+                    <!-- End of col-md-10 -->
+                    </div>
+                <!-- End of content row -->
+                </div>
+
+            <!-- End of hero div -->
+            </div>
+
+        <!-- End of site-wide container -->
+        </div>
+
+
+
+    </div>
+</body>
+
+<!-- Footer -->
+<?php include_once "footer.php"; ?>
+
+</html>
