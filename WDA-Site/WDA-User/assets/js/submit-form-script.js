14d1bc273d009ddb963218b6eed45f526d81703e
jose-phin <josephine.pramudia@gmail.com>
2016-08-23 18:27:48 +1000

    Update submit-form-script.js

--- WDA-Site/WDA-User/assets/js/submit-form-script.js
+++ WDA-Site/WDA-User/assets/js/submit-form-script.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 
 			$.ajax({
 				type: "POST",
-        url: "http://localhost:8888/WDA/ticket/new,
+        url: "http://localhost:8888/WDA/ticket/new",
         contentType: 'application/json',
 				data: JSON.stringify({
 					"user": {
