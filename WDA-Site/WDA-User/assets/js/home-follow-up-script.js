f45fdb130896022ab6b46fe4061798306f9d8565
Dennis <dennishou132@gmail.com>
2016-08-25 12:52:48 +1000

    added base code for ajax api call

--- WDA-Site/WDA-User/assets/js/home-follow-up-script.js
+++ WDA-Site/WDA-User/assets/js/home-follow-up-script.js
@@ -22,5 +22,37 @@ $(document).ready(function(){
         }
     });
 
+    // Process form
+    $(document).submit("#user-login", function() {
+
+  			/* Serialised data */
+  			var data = $("#user-login").serialize();
+
+  			$.ajax({
+  				type: "POST",
+          		url: "/WDA/ticket/new",
+          		contentType: 'application/json',
+  				data: JSON.stringify({
+  			// 		"user": {
+  			// 			"firstName": $("input[name=firstname]").val(),
+  			// 			"lastName": $("input[name=lastname]").val(),
+  			// 			"email": $("input[name=email]").val()
+  			// 		},
+  			// 		"ticket": {
+  			// 			"osType": $("#os").val(),
+  			// 			"primaryIssue": $("#enquiry").val(),
+  			// 			"additionalNotes": $("#description").val()
+  			// 		}
+  				}),
+  				success:  function(formData) {
+  					$("#report-issue").fadeOut(500).hide(function() {
+  						$(".submitted").fadeIn(500).show(function() {
+  							$(".result").text(formData);
+  						});
+  					});
+  				}
+  			});
+  			return false;
+  		});
 
 });
